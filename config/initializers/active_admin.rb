cb911502d9eb6805398044f55370d62ce86572e5
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-01-11 17:31:09 +0300

    added logo

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -3,10 +3,12 @@ ActiveAdmin.setup do |config|
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
-  #
-  config.site_title = "Delizia"
+  
+	config.comments = false
+	config.site_title = "Delizia"
   config.before_filter :set_admin_locale
-  # Set the link url for the title. For example, to take
+  config.site_title_image = "http://dshop24.ru/images/logo.png"
+	# Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
   #
   # config.site_title_link = "/"
