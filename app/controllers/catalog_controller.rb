7498874deea2f89571a37135a0a76f5149809808
Ivan Kabashnikov <vania.kabashnikov@gmail.com>
2016-01-25 14:02:06 +0300

    finalize

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -10,10 +10,36 @@ class CatalogController < ApplicationController
 			@length_min = params[:length_min].to_i
 			@type = params[:type]
 			@zone = params[:zone]
-			@factory = params[:factory]
-			
-			@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:zones).where(:zones => {:name => @zone}).joins(:types).where(:types => {:name => @type}).joins(:factory).where(:factories => {:name => @factory})
-		
+			@factory = params[:factory]	
+			if @type == 'Тип помещения'
+				if @zone == 'Назначение'	
+					if @factory == 'Производитель'
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id)
+					else
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:factory).where(:factories => {:name => @factory})
+					end
+				else
+					if @factory = 'Производитель'			
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:zones).where(:zones => {:name => @zone})
+					else
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:zones).where(:zones => {:name => @zone}).joins(:factory).where(:factories => {:name => @factory})
+					end
+				end
+			else
+				if @zone == 'Назначение'
+					if @factory == 'Производитель'
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:types).where(:types => {:name => @type})
+					else	
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:factory).where(:factories => {:name => @factory}).joins(:types).where(:types => {:name => @type})
+					end
+				else
+					if @factory == 'Производитель'
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:zones).where(:zones => {:name => @zone}).joins(:types).where(:types => {:name => @type})
+					else
+						@goods = Collection.joins(:tiles).where('tiles.width' => @width_min..@width_max, 'tiles.length' => @length_min..@length_max).group(:id).joins(:zones).where(:zones => {:name => @zone}).joins(:types).where(:types => {:name => @type}).joins(:factory).where(:factories => {:name => @factory})
+					end
+				end
+			end
 		else
 			@flag = 1
 			if params[:start_id]
