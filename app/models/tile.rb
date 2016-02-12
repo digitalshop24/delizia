3ddcf88a1fcd3a0c7188c3daa3a63fa3577ab717
K. Kanyuka <kkan.web@gmail.com>
2016-02-12 17:04:01 +0300

    image convert settings

--- app/models/tile.rb
+++ app/models/tile.rb
@@ -3,6 +3,6 @@ class Tile < ActiveRecord::Base
   belongs_to :collection
   belongs_to :surface
   belongs_to :amount
-  has_attached_file :image, :styles => { :medium => "160x160>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "160x160>", :thumb => "100x100>" }, :convert_options => { :medium => '-quality 70 -strip' }
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 end
